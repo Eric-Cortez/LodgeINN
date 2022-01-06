00c989a89caeba452df9685587ec2639ead43ffe
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 10:42:11 -0800

    setup error handling

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,10 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes')
 
+const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
 
 const app = express(); 
 
@@ -36,6 +38,7 @@ app.use(
 
 app.use(routes);
 
+//UNHANDLED REQUEST 
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = "Resource Not Found";
@@ -44,7 +47,25 @@ app.use((_req, _res, next) => {
     next(err);
 });
 
+// SEQUELIZE ERROR-HANDLER 
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
 
-
+//ERROR FORMATTER 
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app; 
