953666c9e3ca01517f6b362e124238f57c47c872
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 10:29:46 -0800

    404 error handling

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,14 @@ app.use(
 
 app.use(routes);
 
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
 
 
 
