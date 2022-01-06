357c45b9e774b3ea223ee8545291eacc889f9d73
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 13:29:20 -0800

    added JWT

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
 
-
+//-----------------------------------------------------------------//
+//TEST ROUTES 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+// TEST JWT TOKEN SET IN COOKIES     
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
+
+//-----------------------------------------------------------------//
+
 
 module.exports = router;
 
