a6e441d7592f51ec3d09e86d5d7bf0bc65e90d89
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 15:22:18 -0800

    created func to handleValidationErrors

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,53 +10,45 @@ router.post('/test', function (req, res) {
 });
 
 
-
-module.exports = router;
-
-
-
-
-
-
-
-
 //-----------------------------------------------------------------//
 
-// const asyncHandler = require('express-async-handler');
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// // TEST JWT TOKEN SET IN COOKIES     
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         },
-//     })
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-// }));
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+// TEST JWT TOKEN SET IN COOKIES     
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+    })
+    setTokenCookie(res, user);
+    return res.json({ user });
+}));
 
-// // TEST RESTORE USER 
-// const { restoreUser } = require('../../utils/auth.js');
-// router.get(
-//     '/restore-user',
-//     restoreUser,
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-
-// //TEST REQUIRE AUTH MIDDLEWARE 
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//     '/require-auth',
-//     requireAuth,
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-
-// //-----------------------------------------------------------------//
-
-
-
+// TEST RESTORE USER 
+const { restoreUser } = require('../../utils/auth.js');
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+        return res.json(req.user);
+    }
+    );
+    
+    //TEST REQUIRE AUTH MIDDLEWARE 
+    const { requireAuth } = require('../../utils/auth.js');
+    router.get(
+        '/require-auth',
+        requireAuth,
+        (req, res) => {
+            return res.json(req.user);
+        }
+        );
+        
+        // //-----------------------------------------------------------------//
+        
+        
+        
+        
+        module.exports = router;
