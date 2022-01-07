dff18afcdd178040cfd3a3c5495fe1ce007af5a0
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 17:49:39 -0800

    setup user session

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-
+import * as sessionActions from './store/session';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 // window.store.dispatch({ type: 'hello' });
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session: sessionReducer,
 });
-
 let enhancer;
 
 if (process.env.NODE_ENV === "production") {
