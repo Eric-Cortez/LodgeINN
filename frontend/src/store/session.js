a26f5e06a81e66cc72a9eefd7374f4dfa8fd5178
Eric-Cortez <ecrcortez@gmail.com>
2022-01-06 19:41:59 -0800

    setup logo

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -54,6 +54,14 @@ export const signup = (user) => async (dispatch) => {
 };
 
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
 // REDUCER 
 const initialState = { user: null };
 
