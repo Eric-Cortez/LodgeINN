22357c625e04e972c551f4e284fd9b82a7723725
Eric-Cortez <ecrcortez@gmail.com>
2022-01-10 21:11:35 -0800

    started edit/PUT route

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -15,9 +15,9 @@ const SpotDetail = () => {
     },[dispatch,spotId])
    
     return (
-        <div>
+        <div className='spot-detail'> 
+            <img className="spot-image" src={oneSpot?.Images[0].url} alt="cabin" /> 
             <h1>{oneSpot?.title}</h1> 
-            <img src={oneSpot?.Images[0].url} alt="cabin" /> 
             <p>{oneSpot?.description}</p>
             <ul> Address: 
                 <li>{oneSpot?.address}</li>
