997c138c3a9404d3631d5e123f92c9dc88c2d4d4
Eric-Cortez <ecrcortez@gmail.com>
2022-01-11 19:44:05 -0800

    re-seed

--- backend/db/seeders/20220108220719-imagesSeeds.js
+++ backend/db/seeders/20220108220719-imagesSeeds.js
@@ -6,19 +6,19 @@ module.exports = {
       return queryInterface.bulkInsert('Images', [
         {
          spotId: 1,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDL6I9H1Ap5leiurWjreSmNPluxqPvp6Atlw&usqp=CAU", 
+          url: "https://images.unsplash.com/photo-1547393429-098dd122091a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNhYmlufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60", 
          createdAt: new Date(),
          updatedAt: new Date()
         },
         {
           spotId: 2,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScBK92RrBBGaaKiywOHF85brKjjcMq8ThdQQ&usqp=CAU",
+          url: "https://images.unsplash.com/photo-1525113990976-399835c43838?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           spotId: 3,
-          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn_oDgA3XdpVGp8siSS4fF6jatkrWcteYfLw&usqp=CAU",
+          url: "https://images.unsplash.com/photo-1587061949409-02df41d5e562?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
           createdAt: new Date(),
           updatedAt: new Date()
         }
