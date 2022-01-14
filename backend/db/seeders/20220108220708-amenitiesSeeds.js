264bc810310c41ce26b72884ea9cc8bae5e6a2a9
Eric-Cortez <ecrcortez@gmail.com>
2022-01-13 18:02:23 -0800

    reseeded

--- backend/db/seeders/20220108220708-amenitiesSeeds.js
+++ backend/db/seeders/20220108220708-amenitiesSeeds.js
@@ -36,10 +36,82 @@ module.exports = {
           parking: true,
           pool: false,
           hotTub: false,
+          pets: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 4,
+          kitchen: true,
+          privateBeachAccess: true,
+          firePlace: true,
+          parking: true,
+          pool: false,
+          hotTub: true,
+          pets: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 5,
+          kitchen: true,
+          privateBeachAccess: true,
+          firePlace: true,
+          parking: true,
+          pool: false,
+          hotTub: true,
+          pets: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 6,
+          kitchen: true,
+          privateBeachAccess: false,
+          firePlace: false,
+          parking: true,
+          pool: false,
+          hotTub: true,
           pets: false,
           createdAt: new Date(),
           updatedAt: new Date()
         },
+        {
+          spotId: 7,
+          kitchen: true,
+          privateBeachAccess: true,
+          firePlace: true,
+          parking: true,
+          pool: true,
+          hotTub: true,
+          pets: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 8,
+          kitchen: true,
+          privateBeachAccess: true,
+          firePlace: true,
+          parking: true,
+          pool: false,
+          hotTub: true,
+          pets: false,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 9,
+          kitchen: true,
+          privateBeachAccess: true,
+          firePlace: true,
+          parking: true,
+          pool: false,
+          hotTub: true,
+          pets: false,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
       ], {});
     
   },
--- backend/db/seeders/20220108220719-imagesSeeds.js
+++ backend/db/seeders/20220108220719-imagesSeeds.js
@@ -12,7 +12,7 @@ module.exports = {
         },
         {
           spotId: 2,
-          url: "https://images.unsplash.com/photo-1525113990976-399835c43838?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+          url: "https://images.unsplash.com/photo-1604609165678-096d20fab1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGNhYmlufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -21,7 +21,44 @@ module.exports = {
           url: "https://images.unsplash.com/photo-1587061949409-02df41d5e562?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
           createdAt: new Date(),
           updatedAt: new Date()
+        },
+        {
+          spotId: 4,
+          url: "https://images.unsplash.com/photo-1548704806-0c20f7ea6474?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bG9kZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 5,
+          url: "https://images.unsplash.com/photo-1536209604112-c4f045f94729?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bG9kZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 6,
+          url: "https://images.unsplash.com/photo-1595521624992-48a59aef95e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGxvZGdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 7,
+          url: "https://images.unsplash.com/photo-1542718610-a1d656d1884c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 8,
+          url: "https://images.unsplash.com/photo-1618140052121-39fc6db33972?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bG9kZ2V8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          spotId: 9,
+          url: "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2FiaW58ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
+        
       ], {});
  
   },
