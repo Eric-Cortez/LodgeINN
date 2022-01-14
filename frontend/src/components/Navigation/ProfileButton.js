9286b6a3526fc2d7daaf9419f84941c0f2c67dc9
Eric-Cortez <ecrcortez@gmail.com>
2022-01-13 21:00:09 -0800

    final fix

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
+    const history = useHistory()
 
     const openMenu = () => {
         if (showMenu) return;
@@ -28,17 +29,18 @@ function ProfileButton({ user }) {
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
+        history.push("/")
     };
 
     return (
         <>
-            <button id="user-btn" onClick={openMenu}>
+            <button className="profile-menu" id="user-btn" onClick={openMenu}>
                 <i className="fas fa-bars" />
                 <i className="fas fa-user-circle" />
             </button>
             {showMenu && (
-                <ul>
-                    <li>{user.username}</li>
+                <ul className="profile-dropdown">
+                    <li>User {user.username}</li>
                     <li>{user.email}</li>
                     <li>
                         {/* <Link to="/spots/host">Host</Link> */}
