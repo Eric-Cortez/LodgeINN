dd1ca18290be0f0d9b75d96aa68a609b7410fde4
Eric-Cortez <ecrcortez@gmail.com>
2022-03-25 03:45:56 -0700

    trip image css fix

--- frontend/src/components/TripsPage/index.js
+++ frontend/src/components/TripsPage/index.js
@@ -65,8 +65,8 @@ const TripPage = () => {
 
 
         <div className='right-trip-div'>
-          {/* <img className='trip-page-image' src='https://images.unsplash.com/photo-1633431305705-c2438b770dbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='trip' /> */}
-          <img className='trip-page-image' src='https://images.unsplash.com/photo-1593053272490-e0ed6d6a42c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='trip' />
+          <img className='trip-page-image' src='https://images.unsplash.com/photo-1633431305705-c2438b770dbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='trip' />
+          {/* <img className='trip-page-image' src='https://images.unsplash.com/photo-1593053272490-e0ed6d6a42c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' alt='trip' /> */}
         </div>
       </div>
       <div className='trip-plans-history'>
