f7ff0277b06e452e4f9a14d7708019b8f4432862
Eric-Cortez <ecrcortez@gmail.com>
2022-03-27 20:20:38 -0700

    added manage booking button on booking confirmation page

--- frontend/src/components/BookingConfirmation/index.js
+++ frontend/src/components/BookingConfirmation/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { dateFormatOrder, dayCount } from '../utils'
 import "../BookingPreviewDetails/BookingPreviewDetails.css"
 import "./BookingConfirmation.css"
@@ -12,18 +12,18 @@ import { getAllBookings } from "../../store/booking"
 const BookingConfirmation = () => {
     const history = useHistory()
     const dispatch = useDispatch()
-    const {userId, bookingId} = useParams()
+    const { userId, bookingId } = useParams()
     console.log(bookingId)
-    
+
     const allSpots = useSelector(state => state?.spots)
     const allUsers = useSelector(state => state?.users)
     const allBookings = useSelector(state => state?.booking?.list)
     const currBooking = allBookings.find(booking => booking?.id === +bookingId)
     const spotId = currBooking?.spotId
     // const allSpotsArr = Object.values(spots)
-   
-    
-    
+
+
+
 
     useEffect(() => {
         dispatch(getAllSpots())
@@ -31,7 +31,7 @@ const BookingConfirmation = () => {
         dispatch(getAllBookings())
     }, [])
 
-    
+
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -40,23 +40,25 @@ const BookingConfirmation = () => {
     function getRandomArbitrary(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-    
+
 
     return (
         <div className='trip-detail-main confirm'>
             <h2 className='trip-detail-head'>Great, You're Booked!</h2>
             <h4 className='confirmation-num'>A confirmation email is on its way to you.</h4>
             <h6 className='confirmation-num'>Confirmation number: {getRandomArbitrary(565565679944233, 100555616446555843632)}</h6>
-            <Link to={`/users/${userId}/trips`}>Manage bookings</Link>
             <div className='trip-info-modal'>
-                <img className="trip-image-small modal" 
-                src={`${allSpots[spotId]?.Images[0]?.url}`} alt="spot" 
-                onError={(e) => { e.target.src = 'https://sonuptraders.com/wp-content/uploads/2019/02/picture-not-available.jpg'; e.target.onError = null; }}/>
+                <img className="trip-image-small modal"
+                    src={`${allSpots[spotId]?.Images[0]?.url}`} alt="spot"
+                    onError={(e) => { e.target.src = 'https://sonuptraders.com/wp-content/uploads/2019/02/picture-not-available.jpg'; e.target.onError = null; }} />
                 <div>
 
                     <h4 className='trip-detail-h4'>Address</h4>
                     <p className='trip-modal-p'>{allSpots[spotId]?.address}</p>
-                    <p className='trip-modal-p'>{allSpots[spotId]?.city}, {allSpots[spotId]?.state} {allSpots[spotId]?.zipCode}</p> 
+                    <p className='trip-modal-p'>{allSpots[spotId]?.city}, {allSpots[spotId]?.state} {allSpots[spotId]?.zipCode}</p>
+                    <div className="listing-link-div">
+                        <Link className="listing-link" to={`/users/${userId}/trips`}>Manage bookings</Link>
+                    </div>
                 </div>
 
             </div>
@@ -65,16 +67,16 @@ const BookingConfirmation = () => {
             <div>
                 <h4 className='trip-detail-h4'>Cabin details</h4>
                 <p className='trip-modal-p'>{allSpots[spotId]?.title}</p>
-                <p className='trip-modal-p'>Hosted by {allUsers[allSpots[spotId]?.userId]?.username}</p> 
+                <p className='trip-modal-p'>Hosted by {allUsers[allSpots[spotId]?.userId]?.username}</p>
             </div>
 
             <div className='booking-line light'></div>
 
             <div>
                 <h4 className='trip-detail-h4'>Travel Dates</h4>
-               {currBooking &&
-                <p className='trip-modal-p'>{dateFormatOrder(new Date(currBooking?.startDate), new Date(new Date(currBooking?.endDate)))}</p>
-               }
+                {currBooking &&
+                    <p className='trip-modal-p'>{dateFormatOrder(new Date(currBooking?.startDate), new Date(new Date(currBooking?.endDate)))}</p>
+                }
             </div>
 
             <div className='booking-line light'></div>
