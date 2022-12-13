a54a08230f0da14a3abd6fb7f432761671d418d1
Eric-Cortez <ecrcortez@gmail.com>
2022-12-12 19:36:53 -0800

    updated databases.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,6 +18,9 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    define: {
+      schema: process.env.SCHEMA
+    },
     seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
@@ -25,8 +28,5 @@ module.exports = {
         rejectUnauthorized: false,
       },
     },
-    define: {  
-      schema: process.env.SCHEMA
-    }
   },
 };
